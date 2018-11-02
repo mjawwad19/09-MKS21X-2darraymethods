bb21853fc896fc16a1c2fa7a0c393e380b039ed0
Mohammad Jawwad <mjawwad@stuy.edu>
2018-11-02 10:56:21 -0400

    Created a basic driver with a 2D array and test cases for rowSum()

---
+++ Drive2D.java
@@ -0,0 +1,15 @@
+public class Drive2D {
+  public static void main(String[] args) {
+    int[][] multiC = new int[][]{
+		{1,0,2,0},
+		{2},
+		{},
+    {1,4,9,2}
+		};
+    System.out.println(ArrayMethods.rowSum(multiC, 0)); //3
+    System.out.println(ArrayMethods.rowSum(multiC, 1)); //2
+    System.out.println(ArrayMethods.rowSum(multiC, 2)); //0
+    System.out.println(ArrayMethods.rowSum(multiC, 3)); //16
+
+  }
+}
