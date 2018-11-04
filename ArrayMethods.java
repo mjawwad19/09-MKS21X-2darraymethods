0b51a23a538bb0e165d37e2e92e7c9825fc6d903
Mohammad Jawwad <mjawwad@stuy.edu>
2018-11-04 14:18:43 -0500

    Reworked and simplified rowSum, without needing a helper since I realized I could just [x][i] to get what I want

--- ArrayMethods.java
+++ ArrayMethods.java
@@ -2,17 +2,14 @@ public class ArrayMethods{
   /*
      *PART 1
      */
-     private static int arraySum(int[]ary) {
-       int sum = 0;
-       for (int i = 0; i <ary.length; i++) {
-         sum+= ary[i];
-       }
-       return sum;
-     }
-     public static int rowSum(int[][] ary, int x){
-       return arraySum(ary[x]);
-     }
-      //returns the sum of the elements in Row x of ary.
+    public static int rowSum(int[][] ary, int x) {
+      if (x > ary.length || x < 0) return 0;
+      int sum = 0;
+      for (int i = 0; i < ary[x].length; i++) {
+        sum += ary[x][i];
+      }
+      return sum;
+    }
 
 
   }
